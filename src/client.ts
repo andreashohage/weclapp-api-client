bd90fca2e2f21cfa0f33ba2863b3324d5263f04b
quantumdream <mail@andreaszeuch.de>
2025-03-05 11:34:20 +0100

    Init

---
+++ src/client.ts
@@ -0,0 +1,72 @@
+import { $Fetch, ofetch } from "ofetch";
+import {
+  Article,
+  CreateTaskPayload,
+  CreateTaskResponse,
+  CreateTimeRecordPayload,
+  CreateTimeRecordResponse,
+  FindArticlesQuery,
+  FindPartiesQuery,
+  FindUsersQuery,
+  Party,
+  Task,
+  User,
+} from "./types";
+
+export class WeclappApiClient {
+  private fetch: $Fetch;
+
+  constructor(baseURL: string, authenticationToken: string) {
+    this.fetch = ofetch.create({
+      baseURL: `${baseURL}/webapp/api/v1`,
+      headers: {
+        AuthenticationToken: authenticationToken,
+      },
+    });
+  }
+
+  async findParties(findPartiesQuery?: FindPartiesQuery) {
+    const { result } = await this.fetch<{ result: Party[] }>("party", {
+      query: findPartiesQuery,
+    });
+
+    return { result };
+  }
+
+  async findUsers(findUsersQuery?: FindUsersQuery) {
+    const { result } = await this.fetch<{ result: User[] }>("user", {
+      query: findUsersQuery,
+    });
+
+    return { result };
+  }
+
+  async findArticles(findArticlesQuery?: FindArticlesQuery) {
+    const { result } = await this.fetch<{ result: Article[] }>("article", {
+      query: findArticlesQuery,
+    });
+
+    return { result };
+  }
+
+  async createTask(createTaskPayload: CreateTaskPayload) {
+    const task = await this.fetch<CreateTaskResponse>("task", {
+      method: "POST",
+      body: createTaskPayload,
+    });
+
+    return task;
+  }
+
+  async createTimeRecord(createTimeRecordPayload: CreateTimeRecordPayload) {
+    const timeRecord = await this.fetch<CreateTimeRecordResponse>(
+      "timeRecord",
+      {
+        method: "POST",
+        body: createTimeRecordPayload,
+      }
+    );
+
+    return timeRecord;
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,214 @@
+export type Party = {
+  id: string;
+  version: string;
+  addresses: [];
+  allowPurchaseOrderCreation: boolean;
+  bankAccounts: [];
+  comissionBlock: boolean;
+  comissionSalesPartners: [];
+  company: string;
+  competitor: boolean;
+  contacts: [];
+  createdDate: number;
+  currencyId: string;
+  currencyName: string;
+  customAttributes: [];
+  customer: boolean;
+  customerAllowDropshippingOrderCreation: boolean;
+  customerBlocked: boolean;
+  customerBusinessType: string;
+  customerDeliveryBlock: boolean;
+  customerInsolvent: boolean;
+  customerInsured: boolean;
+  customerNumber: string;
+  customerSalesChannel: string;
+  customerSalesStageHistory: [];
+  customerUseCustomsTariffNumber: boolean;
+  enableDropshippingInNewSupplySources: boolean;
+  factoring: boolean;
+  fixedResponsibleUser: boolean;
+  formerSalesPartner: boolean;
+  habitualExporter: boolean;
+  invoiceBlock: boolean;
+  lastModifiedDate: number;
+  onlineAccounts: [];
+  optInEmail: boolean;
+  optInLetter: boolean;
+  optInPhone: boolean;
+  optInSms: boolean;
+  partyEmailAddresses: [];
+  partyHabitualExporterLettersOfIntent: [];
+  partyType: string;
+  primaryContactId: string;
+  purchaseViaPlafond: boolean;
+  salesPartner: boolean;
+  supplier: boolean;
+  supplierOrderBlock: boolean;
+  tags: [];
+  topics: [];
+};
+
+export type User = {
+  id: string;
+  version: string;
+  createdDate: number;
+  customAttributes: [];
+  email: string;
+  firstName: string;
+  imageId: string;
+  lastModifiedDate: number;
+  lastName: string;
+  licenses: string[];
+  mobilePhoneNumber: string;
+  status: string;
+  userRoles: [];
+  username: string;
+};
+
+export type Article = {
+  id: string;
+  version: string;
+  active: boolean;
+  applyCashDiscount: boolean;
+  articleAlternativeQuantities: [];
+  articleCalculationPrices: [];
+  articleImages: [];
+  articleNumber: string;
+  articlePrices: [];
+  articleType: string;
+  availableForSalesChannels: [];
+  availableInSale: boolean;
+  batchNumberRequired: boolean;
+  billOfMaterialPartDeliveryPossible: boolean;
+  commissionRate: string;
+  createdDate: number;
+  customAttributes: [];
+  customerArticleNumbers: [];
+  defaultPriceCalculationType: string;
+  defaultStoragePlaces: [];
+  defineIndividualTaskTemplates: boolean;
+  lastModifiedDate: number;
+  lowLevelCode: number;
+  marginCalculationPriceType: string;
+  name: string;
+  packagingUnitBaseArticleId: string;
+  priceCalculationParameters: [];
+  procurementLeadDays: number;
+  productionArticle: boolean;
+  productionBillOfMaterialItems: [];
+  quantityConversions: [];
+  salesBillOfMaterialItems: [];
+  serialNumberRequired: boolean;
+  supplySources: [];
+  tags: [];
+  taxRateType: string;
+  unitId: string;
+  unitName: string;
+  useAvailableForSalesChannels: boolean;
+  useSalesBillOfMaterialItemPrices: boolean;
+  useSalesBillOfMaterialItemPricesForPurchase: boolean;
+  useSalesBillOfMaterialSubitemCosts: boolean;
+};
+
+export type Task = {
+  id: string;
+  version: string;
+  allowOverBooking: boolean;
+  allowTimeBooking: boolean;
+  articleId: string;
+  assignees: [
+    {
+      id: string;
+      version: string;
+      createdDate: number;
+      lastModifiedDate: number;
+      plannedEffort: number;
+      userId: string;
+    }
+  ];
+  billableStatus: boolean;
+  createdDate: number;
+  creatorUserId: string;
+  customAttributes: [];
+  customerId: string;
+  dateFrom: number;
+  dateTo: number;
+  entityReferences: [];
+  lastModifiedDate: number;
+  performanceRecordedStatus: "UNDEFINED";
+  plannedEffort: number;
+  subject: string;
+  taskLists: [];
+  taskPriority: "MEDIUM";
+  taskStatus: "IN_PROGRESS";
+  taskTopics: [];
+  taskTypes: [];
+  taskVisibilityType: "ORGANIZATION";
+  userOfLastStatusChangeId: string;
+  watchers: [];
+};
+
+export type TimeRecord = {
+  id: string;
+  version: string;
+  billable: boolean;
+  billableDurationSeconds: number;
+  billableInvoiceStatus: "NOT_INVOICED";
+  createdDate: number;
+  customAttributes: [{ attributeDefinitionId: string; entityReferences: [] }];
+  description: string;
+  durationSeconds: number;
+  lastModifiedDate: number;
+  performanceRecordedStatus: "NOT_PERFORMANCE_RECORDED";
+  printOnPerformanceRecord: boolean;
+  startDate: number;
+  taskId: string;
+  userId: string;
+  userUsername: string;
+};
+
+export type CreateTaskPayload = {
+  ticketId?: string;
+  dateFrom: number;
+  dateTo: number;
+  taskPriority: "MEDIUM";
+  taskVisibilityType: "ORGANIZATION";
+  taskStatus: "IN_PROGRESS";
+  subject: string;
+  customerId: string;
+  assignees: [{ userId: string; plannedEffort: number }];
+  articleId: string;
+  plannedEffort: number;
+};
+
+export type CreateTaskResponse = Task;
+
+export type CreateTimeRecordPayload = {
+  taskId: string;
+  startDate: number;
+  durationSeconds: number;
+  userId: string;
+  billableDurationSeconds: number;
+  description: string;
+};
+
+export type CreateTimeRecordResponse = TimeRecord;
+
+export type FindPartiesQuery = {
+  "company-eq"?: string;
+  "customerNumber-eq"?: string;
+  page?: number;
+  pageSize?: number;
+};
+
+export type FindUsersQuery = {
+  "email-eq"?: string;
+  page?: number;
+  pageSize?: number;
+};
+
+export type FindArticlesQuery = {
+  page?: number;
+  pageSize?: number;
+  "name-eq"?: string;
+};
